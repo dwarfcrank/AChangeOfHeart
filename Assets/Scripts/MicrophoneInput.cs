096b25eb9862a5943ce799b11b8e12bff5c0ea29
Noe Falzon <NoeFalzon@Noes-MacBook-Pro.local>
2013-01-26 23:05:59 +0200

    Added mic calibration

--- Assets/Scripts/MicrophoneInput.cs
+++ Assets/Scripts/MicrophoneInput.cs
@@ -4,15 +4,22 @@ using System.Collections.Generic;
 
 [RequireComponent(typeof(AudioSource))]
 public class MicrophoneInput : MonoBehaviour {
-	public float sensitivity = 1000;
-	public float threshold = 5;		// 5 seems ok for bare foot
 	
-	float loudness = 0;
+	// sensitivity is adjusted during calibration, so that the noise level roughly reaches
+	// the number in noise_threshold (which is arbitrary and doesn't change). signal_noise_ratio
+	// indicates how much higher than noise_threshold must be the sound input to trigger a beat
+	// detection.
 
+	private float sensitivity = 1000;
+	private const float noise_threshold = 10;
+	public float signal_noise_ratio = 5;
+	
 	List<float> beats;
 
 	public float average_bpm = 0;
 	public int totalBeats;
+
+	public bool calibrating = true;
 
 	float average_bpm_target = 0;
 
@@ -32,10 +39,16 @@ public class MicrophoneInput : MonoBehaviour {
 
 	void Update()
 	{
+		if(calibrating)
+		{
+			UpdateCalibrating();
+			return;
+		}
+
 		float now = Time.time;
 
-		loudness = GetAveragedVolume() * sensitivity;
-		if(loudness > threshold)
+		float loudness = GetAveragedVolume() * sensitivity;
+		if(loudness > noise_threshold * signal_noise_ratio)
 		{
 			if(beats.Count > 0)		// echo protection
 			{
@@ -72,6 +85,10 @@ public class MicrophoneInput : MonoBehaviour {
 
 		// Smoothing
 		average_bpm += (average_bpm_target - average_bpm) * 0.1f;
+		if(average_bpm < 1.0f)
+		{
+			average_bpm = 0.0f;
+		}
 	}
 
 	float GetAveragedVolume()
@@ -84,5 +101,35 @@ public class MicrophoneInput : MonoBehaviour {
 			a += Mathf.Abs(s);
 		}
 		return a/256;
+	}
+
+	private float curr_noise_level = 0;
+
+	private bool wasAbove = false;
+	private bool wasBelow = false;
+
+	void UpdateCalibrating()
+	{
+		float inst_noise_level = GetAveragedVolume() * sensitivity;
+
+		// Instant low pass filtering
+		curr_noise_level += (inst_noise_level - curr_noise_level) * 0.01f;
+
+		if(curr_noise_level < noise_threshold)
+		{
+			wasBelow = true;
+			sensitivity += 10;
+		}
+		else
+		{
+			wasAbove = true;
+			sensitivity -= 10;
+		}
+
+		if(wasAbove && wasBelow)
+		{
+			calibrating = false;
+			Debug.Log("Calibrated to sensitivity: " + sensitivity);
+		}
 	}
 }
