190b165d3f7954982612ab09efb51479b1e85e5b
Matias Juntunen <matias.juntunen@gmail.com>
2013-01-27 02:35:24 +0200

    Added script for score

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class ScoreText : MonoBehaviour {
+	
+	private GUIText guiText;
+	private MicrophoneInput mic;
+	
+	// Use this for initialization
+	void Start () {
+		guiText = gameObject.GetComponent<GUIText>();
+		mic = GameObject.Find ("Beat Detector").GetComponent<MicrophoneInput>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnGUI() {
+		int score = mic.totalBeats - grannyCollision.GetTotalScoreLoss();
+		string text = string.Format("{0}", score);
+		
+		guiText.transform.position = new Vector3(0.0f, 1.0f, 0.0f);
+		guiText.text = text;
+	}
+}
