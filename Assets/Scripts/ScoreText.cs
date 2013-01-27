cd963810444f203aec60a49cc007d3936b043687
Matias Juntunen <matias.juntunen@gmail.com>
2013-01-27 12:27:21 +0200

    Upper left corner score text is now hidden when the player dies.

--- Assets/Scripts/ScoreText.cs
+++ Assets/Scripts/ScoreText.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ScoreText : MonoBehaviour {
 	
 	private GUIText guiText;
+	private GUITexture textBackground;
 	private MicrophoneInput mic;
 	public int score;
 	private Movement player;
@@ -11,16 +12,20 @@ public class ScoreText : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		guiText = gameObject.GetComponent<GUIText>();
+		textBackground = GameObject.Find("ScoreBackground").GetComponent<GUITexture>();
 		mic = GameObject.Find ("Beat Detector").GetComponent<MicrophoneInput>();
 		player = GameObject.Find ("Player").GetComponent<Movement>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		score = player.score;
+		if(player != null) score = player.score;
+		
+		guiText.enabled = !deathCollision.gameOver;
+		textBackground.enabled = !deathCollision.gameOver;
 	}
 	
-	void OnGUI() {		
+	void OnGUI() {
 		string text = string.Format("{0}", score);
 		
 		guiText.text = text;
