956b6d8acce388664dab5918b958b87556695b81
Matias Juntunen <matias.juntunen@gmail.com>
2013-01-27 02:12:01 +0200

    Modified movement script to find the beat detector. :D

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -15,7 +15,7 @@ public class Movement : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-	
+		input = GameObject.Find("Beat Detector").GetComponent<MicrophoneInput>();
 	}
 	
 	// Update is called once per frame
